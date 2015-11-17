91c83e49f0efdc3e34729bf08d18ee9c036440e4
hyc <hyc@detu.com>
2015-11-17 23:54:13 +0800

    just demo

---
+++ app.js
@@ -0,0 +1,16 @@
+var ws = require("nodejs-websocket");
+console.log("开始建立连接...")
+
+var game1 = null,game2 = null , game1Ready = false , game2Ready = false;
+var server = ws.createServer(function(conn){
+    conn.on("text", function (str) {
+        console.log("收到的信息为:"+str)
+        conn.sendText(str)
+    })
+    conn.on("close", function (code, reason) {
+        console.log("关闭连接")
+    });
+    conn.on("error", function (code, reason) {
+        console.log("异常关闭")
+    });
+}).listen(3000)
